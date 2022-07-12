059c1c6ca038eb4d205b1de11ce3969287800b3a
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-07-12 12:00:18 -0500

    v0.6.2 Se ajusta header ajustado

--- template/nav/_head.php
+++ template/nav/_head.php
@@ -7,9 +7,7 @@ $url_assets = (new views())->url_assets;
 ?>
 
 <div class="head-imagen">
-    <img src="<?php echo $url_assets?>img/head/azul.png" id="h_azul">
-    <img src="<?php echo $url_assets?>img/head/rojo.png" id="h_rojo">
-    <img src="<?php echo $url_assets?>img/head/azul_fuerte.png" id="h_azul_fuerte">
+    <img src="<?php echo $url_assets?>img/head/completo.svg" id="header_completo">
     <div class="col-md-12 tg-logo">
         <img src="<?php echo $url_assets?>img/head/tg_logo.png" id="h_logo">
     </div>
