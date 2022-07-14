203c876ee3114943fff729799186674f2f2a73ed
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-07-14 16:25:37 -0500

    v0.19.2 Se añadio head login

---
+++ template/nav/_head_login.php
@@ -0,0 +1,14 @@
+<?php
+/** @var stdClass $data Obtenido de index de la funcion data para la definicion del men  */
+use config\views;
+
+$path_base_template = (new views())->ruta_templates;
+$url_assets = (new views())->url_assets;
+?>
+
+<div class="head-imagen">
+    <img src="<?php echo $url_assets?>img/head/completo_6.svg" id="header_completo">
+    <div class="col-md-12 tg-logo">
+        <img src="<?php echo $url_assets?>img/head/tg_logo.png" id="h_logo">
+    </div>
+</div>
