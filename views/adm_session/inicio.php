8e4fee98ff12b1033c2a50ed5f456884a12d8446
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-07-14 18:50:34 -0500

    v0.23.2 Se agrego vista de inicio

--- views/adm_session/inicio.php
+++ views/adm_session/inicio.php
@@ -1,19 +1,35 @@
 <?php /** @var controllers\controlador_adm_session $controlador */
 
-use gamboamartin\system\links_menu; ?>
+use config\views;
+use gamboamartin\system\links_menu;
+$url_assets = (new views())->url_assets;
+?>
 
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="top-title">
-                <ul class="breadcrumb">
-                    <li class="item"> Inicio </a></li>
-                    <li class="item"><a href="<?php echo (new links_menu($controlador->registro_id))->links->adm_session->logout ;?>"> Salir </a></li>
-                    <li class="item"> Bienvenido</li>
-                </ul>
-                <h1 class="h-side-title page-title page-title-big text-color-primary">Bienvenido a Catalogos SAT</h1>
-            </div> <!-- /. content-header -->
-            <!-- /. widget-AVAILABLE PACKAGES -->
-        </div><!-- /.center-content -->
+            <div class="cont_text_inicio">
+                <h1 class="h-side-title page-title page-title-big text-color-primary">Hola, Nombre Completo</h1>
+                <h1 class="h-side-title page-title text-color-primary">Da click en la sección que deseas utilizar</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-sm-2">
+                <a href="./index.php?seccion=org_empresa&accion=alta&session_id=<?php echo $controlador->session_id;?>">
+                    <div class="cont_imagen_accion">
+                        <img src="<?php echo $url_assets?>img/inicio/imagen_1.jpg">
+                    </div>
+                    <div class="cont_text_accion">
+                        <h4 class="text_seccion">Empresas</h4>
+                        <h4 class="text_accion">Alta</h4>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 </div>
