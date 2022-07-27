3e37f21857323340d14c56804a5a61b5260f3de4
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-27 10:29:31 -0500

    v0.42.5 Se refactoriza template tg

--- src/directivas.php
+++ src/directivas.php
@@ -11,53 +11,6 @@ class directivas extends \gamboamartin\template\directivas {
 
     }
 
-    /**
-     * Genera un boton par ala validacion de una persona fisica
-     * @param int $registro_id Registro a aplicar transaccion
-     * @param string $valida_persona_fisica Valor actual del registro
-     * @return array|string
-     */
-    public function button_href_valida_persona_fisica(int $registro_id, string $valida_persona_fisica): array|string
-    {
-
-        $style = 'danger';
-        if($valida_persona_fisica === 'activo'){
-            $style = 'info';
-        }
-
-        $html = $this->button_href(accion: 'valida_persona_fisica',etiqueta: $valida_persona_fisica
-            ,name: 'valida_persona_fisica', place_holder: 'Valida persona fisica',registro_id: $registro_id,
-            seccion: 'cat_sat_tipo_persona', style: $style);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al generar label', data: $html);
-        }
-        return $html;
-    }
-
-
-
-    /**
-     * Genera un input de tipo alias
-     * @param stdClass $row_upd Registro obtenido para actualizar
-     * @version 0.45.7
-     * @param bool $value_vacio Para altas en caso de que sea vacio o no existe el key
-     * @return array|string
-     */
-    public function input_alias(stdClass $row_upd, bool $value_vacio): array|string
-    {
-        $html =$this->input_text_required(disable: false,name: 'alias',
-            place_holder: 'Alias', row_upd: $row_upd, value_vacio: $value_vacio);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al generar input', data: $html);
-        }
-
-        $div = $this->html->div_group(cols: 6,html:  $html);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
-        }
-
-        return $div;
-    }
 
     /**
      * Genera un input de tipo codigo
@@ -78,104 +31,6 @@ class directivas extends \gamboamartin\template\directivas {
 
         $html =$this->input_text_required(disable: false,name: 'codigo',place_holder: 'Codigo',row_upd: $row_upd,
             value_vacio: $value_vacio);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al generar input', data: $html);
-        }
-
-        $div = $this->html->div_group(cols: $cols,html:  $html);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
-        }
-
-        return $div;
-    }
-
-    /**
-     * Genera un input de tipo codigo bis
-     * @version 0.32.5
-     * @param int $cols Numero de columnas boostrap
-     * @param stdClass $row_upd Registro obtenido para actualizar
-     * @param bool $value_vacio Para altas en caso de que sea vacio o no existe el key
-     * @return array|string
-     */
-    public function input_codigo_bis(int $cols, stdClass $row_upd, bool $value_vacio): array|string
-    {
-
-        $valida = $this->valida_cols(cols: $cols);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
-        }
-
-        $html =$this->input_text_required(disable: false,name: 'codigo_bis',
-            place_holder: 'Codigo BIS', row_upd: $row_upd, value_vacio: $value_vacio);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al generar input', data: $html);
-        }
-        $div = $this->html->div_group(cols: $cols,html:  $html);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
-        }
-
-        return $div;
-
-    }
-
-    /**
-     * Genera un text de tipo descripcion
-     * @version 0.40.5
-     * @param stdClass $row_upd Objeto con datos del row
-     * @param bool $value_vacio si value vacia no integra valor en el input
-     * @return array|string
-     */
-    public function input_descripcion(stdClass $row_upd, bool $value_vacio): array|string
-    {
-        $html =$this->input_text_required(disable: false,name: 'descripcion', place_holder: 'Descripcion',
-            row_upd: $row_upd, value_vacio: $value_vacio);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al generar input', data: $html);
-        }
-
-        $div = $this->html->div_group(cols: 12,html:  $html);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
-        }
-
-        return $div;
-
-    }
-
-    /**
-     * Genera un input text de descripcion_select
-     * @param stdClass $row_upd Registro obtenido para actualizar
-     * @param bool $value_vacio Para altas en caso de que sea vacio o no existe el key
-     * @return array|string
-     */
-    public function input_descripcion_select(stdClass $row_upd, bool $value_vacio): array|string
-    {
-        $html =$this->input_text_required(disable: false,name: 'descripcion_select',
-            place_holder: 'Descripcion Select', row_upd: $row_upd, value_vacio: $value_vacio);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al generar input', data: $html);
-        }
-
-        $div = $this->html->div_group(cols: 6,html:  $html);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
-        }
-
-        return $div;
-    }
-
-    /**
-     * @param int $cols Numero de columnas css
-     * @param stdClass $row_upd
-     * @param bool $value_vacio
-     * @return array|string
-     */
-    public function input_id(int $cols, stdClass $row_upd, bool $value_vacio): array|string
-    {
-        $html =$this->input_text(disable: true,name: 'id',place_holder: 'ID',
-            required: false, row_upd: $row_upd, value_vacio: $value_vacio);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar input', data: $html);
         }
@@ -226,44 +81,6 @@ class directivas extends \gamboamartin\template\directivas {
 
         return $div;
 
-    }
-
-
-    /**
-     * Genera un mensaje de exito
-     * @param string $mensaje_exito
-     * @return array|string
-     * @version 0.13.0
-     */
-    public function mensaje_exito(string $mensaje_exito): array|string
-    {
-        $alert_exito = '';
-        if($mensaje_exito!==''){
-            $alert_exito = $this->html->alert_success(mensaje: $mensaje_exito);
-            if(errores::$error){
-                return $this->error->error(mensaje: 'Error al generar alerta', data: $alert_exito);
-            }
-
-        }
-        return $alert_exito;
-    }
-
-    /**
-     * Genera un mensaje de tipo warning
-     * @param string $mensaje_warning
-     * @return array|string
-     * @version 0.19.1
-     */
-    public function mensaje_warning( string $mensaje_warning): array|string
-    {
-        $alert_warning = '';
-        if($mensaje_warning!==''){
-            $alert_warning = $this->html->alert_warning(mensaje: $mensaje_warning);
-            if(errores::$error){
-                return $this->error->error(mensaje: 'Error al generar alerta', data: $alert_warning);
-            }
-        }
-        return $alert_warning;
     }
 
 
