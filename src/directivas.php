fcdfbec6f8b2bc53db5c8379cb3492cd39763c88
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-02-01 16:37:54 -0600

    v3.3.0 Se ajusta html inputs

--- src/directivas.php
+++ src/directivas.php
@@ -11,36 +11,4 @@ class directivas extends \gamboamartin\template\directivas {
 
     }
 
-
-    /**
-     * Genera un input de tipo codigo
-     * @param int $cols Numero de columnas boostrap
-     * @version 0.32.4
-     * @param stdClass $row_upd Registro obtenido para actualizar
-     * @param bool $value_vacio Para altas en caso de que sea vacio o no existe el key
-     * @return array|string
-     */
-    public function input_codigo(int $cols, stdClass $row_upd, bool $value_vacio): array|string
-    {
-
-        $valida = $this->valida_cols(cols: $cols);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al validar columnas ', data: $valida);
-        }
-
-
-        $html =$this->input_text_required(disabled: false,name: 'codigo',place_holder: 'Codigo',row_upd: $row_upd,
-            value_vacio: $value_vacio);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al generar input', data: $html);
-        }
-
-        $div = $this->html->div_group(cols: $cols,html:  $html);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
-        }
-
-        return $div;
-    }
-
 }
