7c6b5384d779c95a9ad384334f999dcae65726d8
Kevin <kevin.acuna@ivitec.com.mx>
2023-03-14 15:48:51 -0500

    4.4.0 Se ajusta posicion de elementos en views

--- assets/css/_base_css.php
+++ assets/css/_base_css.php
@@ -4,9 +4,14 @@ $ruta_template_base = (new views())->ruta_template_base;
 include $ruta_template_base.'/assets/css/base.php';
 ?>
 <style>
+    .container{
+        width: 80%;
+    }
+
     .catalogos{
         display: grid;
-        grid-template-columns: repeat(6, 1fr);
+        grid-template-columns: repeat(7, 1fr);
         grid-gap: 15px;
     }
+
 </style>
