b4df184161dcf7be93dba1b1ec3c9fe0db784525
Kevin <kevin.acuna@ivitec.com.mx>
2023-03-14 14:54:04 -0500

    v4.3.0 Se ajusta dispocision de elementos

--- assets/css/_base_css.php
+++ assets/css/_base_css.php
@@ -2,3 +2,11 @@
 use config\views;
 $ruta_template_base = (new views())->ruta_template_base;
 include $ruta_template_base.'/assets/css/base.php';
+?>
+<style>
+    .catalogos{
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        grid-gap: 15px;
+    }
+</style>
