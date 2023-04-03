459d2ea795ac9590ba24695f93aba6762210e700
Kevin <kevin.acuna@ivitec.com.mx>
2023-04-03 13:14:35 -0500

    v4.12.0 Se ajusta load.js

--- assets/js/load_table.js
+++ assets/js/load_table.js
@@ -1,7 +1,12 @@
 var loading = false;
 var pagina = 1;
 
+let seccion = getParameterByName('seccion');
+const ruta_load = get_url(seccion, "load_table", {});
+
 $(window).load(function () {
+    console.log(ruta_load)
+
     load_contenido();
 });
 
@@ -38,11 +43,31 @@ const load_contenido = () => {
         contentType: false,
         processData: false,
         success: function (response) {
+            console.log(response)
+
             if (response.status === 'Success') {
                 $('#load').html(response.html);
             } else if (response.status === 'Error') {
                 alert("Ha ocurrido un error: " + response.message);
+            } else {
+                console.log(response)
             }
         },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            let response = XMLHttpRequest.responseText;
+            response = response.replace('[]', '')
+
+            if (response.includes('Acceso denegado')) {
+                const html = `<div class="col-md-9 info-lista" style="margin-top: 20px;"><div class="row-cols-12">
+                                <div class="alert alert-danger" role="alert">
+                                    <strong>Oh error!</strong> Acceso denegado - No cuenta con los permisos necesarios para la ruta: <strong>(${ruta_load})</strong>.
+                                </div>
+                              </div></div>`;
+
+                $('#load').html(html);
+            } else {
+                alert("Ha ocurrido un error: " + textStatus);
+            }
+        }
     });
 };
