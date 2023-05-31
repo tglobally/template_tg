b24f0d95c8bdc36e40f8d675debf3b9fedee134f
Kevin <kevin.acuna@ivitec.com.mx>
2023-05-31 11:44:10 -0500

    v4.52.0 Se ajusta main

--- template/template_1/structure/_main.php
+++ template/template_1/structure/_main.php
@@ -6,42 +6,47 @@
     <div class="body flex-grow-1 px-3 d-flex">
         <div class="container-lg">
 
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb mb-4">
-                    <li class="breadcrumb-item">
-                        <a href="./index.php?seccion=adm_session&accion=inicio&session_id=<?php echo $controlador->session_id ?>">Inicio</a>
-                    </li>
+            <?php if ($controlador->accion != 'inicio') : ?>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item">
+                            <a href="./index.php?seccion=adm_session&accion=inicio&session_id=<?php echo $controlador->session_id ?>">Inicio</a>
+                        </li>
 
-                    <?php if ($controlador->accion === 'lista') :?>
-                        <li class="breadcrumb-item active" aria-current="page">Lista</li>
-                    <?php  else:;?>
-                        <li class="breadcrumb-item"><a href="<?php echo $controlador->link_lista?>">Lista</a></li>
-                        <li class="breadcrumb-item active" aria-current="page"><?php echo $controlador->accion_titulo?></li>
-                    <?php   endif;?>
-                </ol>
-            </nav>
+                        <?php if ($controlador->accion === 'lista') : ?>
+                            <li class="breadcrumb-item active" aria-current="page">Lista</li>
+                        <?php else:; ?>
+                            <li class="breadcrumb-item"><a href="<?php echo $controlador->link_lista ?>">Lista</a></li>
+                            <li class="breadcrumb-item active"
+                                aria-current="page"><?php echo $controlador->accion_titulo ?></li>
+                        <?php endif; ?>
+                    </ol>
+                </nav>
+            <?php endif; ?>
 
             <div class="row">
                 <div class="col-xl-12">
                     <div class="card mb-4">
                         <div class="card-body p-4">
 
-                            <div class="row">
-                                <div class="col">
-                                    <div class="card-title fs-2 fw-semibold"><?php echo $controlador->titulo_accion?></div>
+                            <?php if ($controlador->accion != 'inicio') : ?>
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="card-title fs-2 fw-semibold"><?php echo $controlador->titulo_accion ?></div>
+                                    </div>
+                                    <div class="col-auto ms-auto">
+                                        <button class="btn btn-secondary btn-sm" type="button"
+                                                data-coreui-toggle="dropdown" aria-expanded="false">
+                                            <i class="bi bi-list-task icon me-2"></i>
+                                            Acciones
+                                        </button>
+                                        <ul class="dropdown-menu dropdown-menu-end">
+                                            <?php echo $controlador->html_acciones_menu ?>
+                                        </ul>
+                                    </div>
                                 </div>
-                                <div class="col-auto ms-auto">
-                                    <button class="btn btn-secondary btn-sm" type="button" data-coreui-toggle="dropdown" aria-expanded="false">
-                                        <i class="bi bi-list-task icon me-2"></i>
-                                        Acciones
-                                    </button>
-                                    <ul class="dropdown-menu dropdown-menu-end">
-                                        <li><a class="dropdown-item" href="<?php echo $controlador->link_alta?>">Alta</a></li>
-                                        <li><a class="dropdown-item" href="<?php echo $controlador->link_im_movimiento_sube_archivo?>">Importar Registros</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <?php  include($data->include_action); ?>
+                            <?php endif; ?>
+                            <?php include($data->include_action); ?>
                         </div>
                     </div>
                 </div>
