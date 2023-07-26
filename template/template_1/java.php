c8022c60e7f8073e39a0e90cc93afdc6c6b3212f
Kevin <kevin.acuna@ivitec.com.mx>
2023-07-26 11:46:04 -0500

    v4.56.0 Se ajustan dependencias requeridas

--- template/template_1/java.php
+++ template/template_1/java.php
@@ -1,9 +1,26 @@
+<!--
+<script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+<script src="https://coreui.io/demos/bootstrap/4.3/light-v3/vendors/@coreui/coreui-pro/js/coreui.bundle.min.js"></script>
+<script src="https://coreui.io/demos/bootstrap/4.3/light-v3/vendors/simplebar/js/simplebar.min.js"></script>
+<script src="<?php /*echo (new views())->url_js; */?>base.js" type="text/javascript"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+<script src="<?php /*echo (new views())->url_assets; */?>js/datatables_init.js.php"></script>
+<script src="https://gyrocode.github.io/jquery-datatables-checkboxes/1.2.10/js/dataTables.checkboxes.min.js"></script>
+<script src="<?php /*echo $url*/?>base.js"></script>-->
+
 
 <?php use config\generales;
 use config\views;
 
 $url = (new generales())->url_base.'vendor/tglobally/template_tg/template/template_1/js/';
-
 
 ?>
 <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
@@ -19,6 +36,19 @@ $url = (new generales())->url_base.'vendor/tglobally/template_tg/template/templa
 <script src="<?php echo (new views())->url_js; ?>base.js" type="text/javascript"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+<script src="<?php echo (new views())->url_js; ?>funciones.js" type="text/javascript"></script>
+<script src="<?php echo (new views())->url_js; ?>loader.js" type="text/javascript"></script>
+<script src="<?php echo (new views())->url_assets; ?>js/load_table.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.colVis.min.js"></script>
+<script src="https://cdn.datatables.net/select/1.6.0/js/dataTables.select.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/imask/6.4.3/imask.min.js"
+        integrity="sha512-yJ3vm1HmQtcgeMtbUYCp7PuTLyjU+ffCnVNTuE1Uccv1BmkoaJIXt1EjBVGnscjCILc62hTJJJ2rJJBTcw8RjQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://unpkg.com/read-excel-file@5.x/bundle/read-excel-file.min.js"></script>
 <script src="<?php echo (new views())->url_assets; ?>js/datatables_init.js.php"></script>
 <script src="https://gyrocode.github.io/jquery-datatables-checkboxes/1.2.10/js/dataTables.checkboxes.min.js"></script>
 <script src="<?php echo $url?>base.js"></script>
+
+
+
+
